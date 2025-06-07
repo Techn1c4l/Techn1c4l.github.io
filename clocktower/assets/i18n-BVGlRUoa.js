a0a78543236ddaa5eedc36ea56fea7fb1400c995
Techn1c4l <80050457+Techn1c4l@users.noreply.github.com>
2025-06-07 19:24:01 +0500

    Update

---
+++ clocktower/assets/i18n-BVGlRUoa.js
@@ -0,0 +1,1 @@
+import{c as l,m as o}from"./index-CZq9qRTg.js";import"./index-CHOguusj.js";const n=({app:e})=>{const a=l({locale:"en-US",fallbackLocale:"en-US",legacy:!1,globalInjection:!0,messages:o});e.use(a)};export{n as default};
---
+++ scriptorium/assets/ApplicationInfo-Bw2Dx_Q2.js
@@ -0,0 +1,1 @@
+class r{static get Title(){return"Scriptorium"}static get Author(){return"Technical"}static get Version(){return"1.0.0"}}export{r as A};
