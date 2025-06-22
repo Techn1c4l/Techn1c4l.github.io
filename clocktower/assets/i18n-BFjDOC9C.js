d6a69d6e82b30f3198cecef5d813f7c2f4dac31b
Techn1c4l <80050457+Techn1c4l@users.noreply.github.com>
2025-06-22 12:00:39 +0500

    Update

---
+++ clocktower/assets/i18n-BFjDOC9C.js
@@ -0,0 +1,1 @@
+import{c as l,m as o}from"./index-C2bld0Mg.js";import"./index-B5bz1C-x.js";const n=({app:e})=>{const a=l({locale:"en-US",fallbackLocale:"en-US",legacy:!1,globalInjection:!0,messages:o});e.use(a)};export{n as default};
