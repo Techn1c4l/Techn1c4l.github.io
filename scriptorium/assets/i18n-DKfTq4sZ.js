40855290942b67b15b8876ff9a27e6ca71427361
Techn1c4l <80050457+Techn1c4l@users.noreply.github.com>
2025-06-23 13:28:27 +0500

    Update

--- scriptorium/assets/i18n-DKfTq4sZ.js
+++
@@ -1,1 +0,0 @@
-import{c as l,m as o}from"./index-BxsaJrav.js";import"./index-BSlLIg3L.js";const n=({app:e})=>{const a=l({locale:"en-US",fallbackLocale:"en-US",legacy:!1,globalInjection:!0,messages:o});e.use(a)};export{n as default};
